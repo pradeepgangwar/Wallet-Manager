11da21a28ac8ce1d4a8ce605d9f66e8204e2a425
pradeepgangwar <pradeepgangwar39@gmail.com>
2017-04-06 01:14:56 +0530

    Corrected wrong input in month bug

--- static/js/main.js
+++ static/js/main.js
@@ -8,5 +8,12 @@ $( document ).ready(function(){
 	$('.carousel').carousel('prev', 4); // Move prev n times.
 	// Set to nth slide
 	$('.carousel').carousel('set', 4);
+	$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 50 // Creates a dropdown of 15 years to control year
+  });
+	$(document).ready(function() {
+    $('select').material_select();
+  });
 })
 
